a86613231304b566789cfb5316bf9597e01487c1
Sheenan Tenepre <sheenan.tenepre@shiftbrain.co.jp>
2016-03-31 19:26:05 +0900

    update config

--- src/config/router.js
+++ src/config/router.js
@@ -9,15 +9,13 @@ import ConfirmBattleContainer from '../containers/ConfirmBattleContainer'
 import ResultsContainer from '../containers/ResultsContainer'
 
 let routes = (
-    <Router history={ hashHistory }>
-        <Route path='/' component={ Main }>
-            <IndexRoute component={ Home } />
-            <Route path='playerOne' header='Player One' component={ PromptContainer } />
-            <Route path='playerTwo/:playerOne' header='Player Two' component={ PromptContainer } />
-            <Route path='battle' component={ ConfirmBattleContainer } />
-            <Route path='results' component={ ResultsContainer } />
-        </Route>
-    </Router>
+    <Route path='/' component={ Main }>
+        <IndexRoute component={ Home } />
+        <Route path='playerOne' header='Player One' component={ PromptContainer } />
+        <Route path='playerTwo/:playerOne' header='Player Two' component={ PromptContainer } />
+        <Route path='battle' component={ ConfirmBattleContainer } />
+        <Route path='results' component={ ResultsContainer } />
+    </Route>
 );
 
 export default routes
